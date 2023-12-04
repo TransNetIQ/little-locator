74057e0385a54e4c8a5613623bd3514730b91165
Mark CDA <aclo.create@gmail.com>
2023-12-05 00:49:36 +0300

    Добавлено поле времени в `Location`

--- data_types/src/lib.rs
+++ data_types/src/lib.rs
@@ -1,6 +1,5 @@
+use chrono::{serde::ts_microseconds, DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, Mutex, OnceLock};
-use std::sync::mpsc;
 
 /// Данные о местоположении.
 #[derive(Deserialize, Serialize, Clone)]
@@ -9,14 +8,17 @@ pub struct Location {
   pub x: f32,
   pub y: f32,
   pub z: f32,
+  #[serde(default = "curr_ts")]
+  #[serde(with = "ts_microseconds")]
+  pub ts: DateTime<Utc>,
+}
+
+fn curr_ts() -> DateTime<Utc> {
+  chrono::Utc::now()
 }
 
 impl std::fmt::Display for Location {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    f.write_str(&format!("Местоположение объекта #{}: x - {}, y - {}, z - {}", self.id, self.x, self.y, self.z))
+    f.write_str(&format!("Местоположение объекта #{}: x - {}, y - {}, z - {}; информация получена {}.", self.id, self.x, self.y, self.z, self.ts))
   }
 }
-
-// Ячейка для обмена данными между бэкендом и фронтендом.
-pub type DataQueue<T> = Arc<Mutex<mpsc::Sender<T>>>;
-pub static DATA_QUEUE: OnceLock<DataQueue<Location>> = OnceLock::new();
