3f12354ff219c053b84204932ea209de06a80ff2
Mark CDA <aclo.create@gmail.com>
2023-12-05 00:51:21 +0300

    Дополнена документация

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -11,20 +11,7 @@ use rfd::AsyncFileDialog;
 #[cfg(not(target_arch = "wasm32"))]
 use rfd::FileDialog;
 
-// use web_sys::{ErrorEvent, MessageEvent, WebSocket};
-
-/// Загружает изображение из файла.
-// fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
-//   let image = image::io::Reader::open(path)?.decode()?;
-//   let size = [image.width() as _, image.height() as _];
-//   let image_buffer = image.to_rgba8();
-//   let pixels = image_buffer.as_flat_samples();
-//   Ok(egui::ColorImage::from_rgba_unmultiplied(
-//     size,
-//     pixels.as_slice(),
-//   ))
-// }
-
+/// Загружает изображение из массива байтов.
 fn load_image_from_memory(image_data: &[u8]) -> Result<egui::ColorImage, image::ImageError> {
   let image = image::load_from_memory(image_data)?;
   let size = [image.width() as _, image.height() as _];
